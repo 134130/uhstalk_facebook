2c932d80fe91ea7c54ab71d3cc3e448822c69343
root <root@ip-172-31-41-144.ap-northeast-2.compute.internal>
2020-02-29 01:42:37 +0900

    open chat facebook

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 'use strict'
 
 const http = require('http');
-setInterval(function() {
+setInterval(function () {
     http.get("http://agile-castle-50630.herokuapp.com/");
 }, 6000000);
 
@@ -59,20 +59,65 @@ app.post('/webhook', function (req, res) {
 function receivedMessage(event) {
     var senderId = event.sender.id;
     var content = event.message.text;
-    
-    if (content == '1') {
-        randomChatInit(senderId);
+
+    if (senderId in openChatDict) {
+        if (content == '/나가기') {
+            delete openChatDict[senderId];
+        } else {
+            setInterval(() => {
+                for (var key in openChatDict) {
+                    var str = openChatDict[senderId] + ": " + content;
+                    sendTextMessage(key, str);
+                }
+            }, 1);
+        }
+    } else {
+        if (senderId in openChatDict2) {
+            openChatDict[senderId] = content;
+            delete openChatDict2[senderId];
+        }
+        else if (content =='/입장') {
+            setInterval(sendTextMessage, 1, senderId, '닉네임을 입력해주세요');
+            openChatDict2[senderId] = 1;
+        } else {
+            sendTextMessage(senderId, "협성대학교 재학생들의 오픈채팅방입니다. /입장 /나가기 를 통해 이용해주세요~");
+        }
     }
 
-    sendTextMessage(senderId, '1. 랜덤채팅');
+    /*if (!randomChatQueue.dataStore.includes(senderId)) {
+        if (content == '1') {
+            randomChatInit(senderId);
+        } else {
+            sendTextMessage(sendrId, '1. 랜덤채팅시작');
+        }
+    }
+    else {
+        
+    }*/
 }
 
+// senderId: Nickname
+var openChatDict = {};
+var openChatDict2 = {};
+
+/******
 var randomChatQueue = new Queue();
+var randomChatDict = {};
 function randomChatInit(senderId) {
-    randomChatQueue.enqueue(senderId);
-    var str = randomChatQueue.dataStore.length.toString() + '명과 랜덤매칭 시작!';
+    var str = '대기중이야 조금만 기다려!';
     setTimeout(sendTextMessage, 1, senderId, str);
+    setTimeout(function() {
+        if (randomChatQueue.dataStore.length >= 1) {
+            var user = randomChatQueue.dequeue();
+            sendTextMessage(senderId, '매칭 완료!');
+            sendTextMessage(user, '매칭 완료!');
+        } else {
+            randomChatQueue.enqueue(senderId);
+        }
+    }, 1);
 }
+
+********/
 
 function receivedPostback(event) {
     var senderId = event.sender.id;
@@ -106,7 +151,6 @@ function sendTextMessage(recipientId, message) {
     });
 }
 
-
 function Queue() {
     this.dataStore = [];
     this.enqueue = function enqueue(element) {
@@ -119,17 +163,17 @@ function Queue() {
         return this.dataStore[0]
     };
     this.back = function back() {
-        return this.dataStore[this.dataStore.length-1];
+        return this.dataStore[this.dataStore.length - 1];
     };
     this.toString = function toString() {
         var retStr = "";
-        for (var i=0; i<this.dataStore.length; ++i) {
+        for (var i = 0; i < this.dataStore.length; ++i) {
             retStr += this.dataStore[i] + "\n";
         }
         return retStr;
     }
     this.empty = function empty() {
-        if(this.dataStore.length == 0) {
+        if (this.dataStore.length == 0) {
             return true;
         } else return false;
     }
