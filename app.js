6e66c626509a9a27b16d65627c94bf3f05cb4386
root <root@ip-172-31-41-144.ap-northeast-2.compute.internal>
2020-02-29 23:45:05 +0900

    added ranking system and randomTactGame

--- app.js
+++ app.js
@@ -59,72 +59,141 @@ function receivedMessage(event) {
     var senderId = event.sender.id;
     var content = event.message.text;
 
-    console.log("receive(",senderId,"): ", content);
+    console.log("receive(", senderId, "): ", content);
 
-    if (senderId in openChatDict) {
-        if (content == '/나가기') {
-            delete openChatDict[senderId];
-            for (var key in openChatDict) {
-                var str = openChatDict[senderId] + "님이 채팅방을 나갔어요";
-                sendTextMessage(key, str);
+    switch (content) {
+        case '/입장':
+            waitOpenChat(senderId);
+            break;
+        case '/나가기':
+            quitOpenChat(senderId);
+            break;
+
+        case '/랭킹':
+            var str = "인사대: " + collegeRank[0] + "\n경영대: " + collegeRank[1] + "\n이공대: " + collegeRank[2] + "\n예술대: " + collegeRank[3] + "\n신학대: " + collegeRank[4];
+            sendTextMessage(senderId, str);
+        case '/인사대':
+            rankingGame(senderId, 0);
+            break;
+        case '/경영대':
+            rankingGame(senderId, 1);
+            break;
+        case '/이공대':
+            rankingGame(senderId, 2);
+            break;
+        case '/예술대':
+            rankingGame(senderId, 3);
+            break;
+        case '/신학대':
+            rankingGame(senderId, 4);
+            break;
+
+        default:
+            if (senderId in openChatDict2) {
+                enterOpenChat(senderId, content);
+                break;
+            } else if (senderId in openChatDict) {
+                randomTactGame();
+                sendTextMessage(senderId, content);
+                break;
+            } else {
+                sendTextMessage(senderId, "협성대학교 재학생들의 오픈채팅방입니다. /입장 /나가기 를 통해 이용해주세요~");
+                break;
             }
-        } else {
-            for (var key in openChatDict) {
-                var str = openChatDict[senderId] + ": " + content;
-                sendTextMessage(key, str);
-            }
-        }
-    } else {
-        if (senderId in openChatDict2) {
-            openChatDict[senderId] = content;
-            delete openChatDict2[senderId];
-            for (var key in openChatDict) {
-                var str = openChatDict[senderId] + "님이 채팅방에 입장했어요";
-                sendTextMessage(key, str);
-            }
-        }
-        else if (content =='/입장') {
-            sendTextMessage(senderId, '닉네임을 입력해주세요');
-            openChatDict2[senderId] = 1;
-        } else {
-            sendTextMessage(senderId, "협성대학교 재학생들의 오픈채팅방입니다. /입장 /나가기 를 통해 이용해주세요~");
-        }
     }
 
-    /*if (!randomChatQueue.dataStore.includes(senderId)) {
-        if (content == '1') {
-            randomChatInit(senderId);
+    if (content == '/입장' && !(senderId in openChatDict) && !(senderId in openChatDict2)) {
+        enterOpenChat(senderId);
+    } else if (content == )
+
+        if (senderId in openChatDict) {
+            if (content == '/나가기') {
+                delete openChatDict[senderId];
+                11111111111111111111111111111111
+                for (var key in openChatDict) {
+                    var str = openChatDict[senderId] + "님이 채팅방을 나갔어요";
+                    sendTextMessage(key, str);
+                }
+            } else {
+                for (var key in openChatDict) {
+                    var str = openChatDict[senderId] + ": " + content;
+                    sendTextMessage(key, str);
+                }
+            }
         } else {
-            sendTextMessage(sendrId, '1. 랜덤채팅시작');
+            if (senderId in openChatDict2) {
+                openChatDict[senderId] = content;
+                111111111111111111111111111
+                delete openChatDict2[senderId];
+                for (var key in openChatDict) {
+                    var str = openChatDict[senderId] + "님이 채팅방에 입장했어요";
+                    sendTextMessage(key, str);
+                }
+            } else if (content == '/입장') {
+                sendTextMessage(senderId, '닉네임을 입력해주세요');
+                openChatDict2[senderId] = 1;
+            } else {
+
+            }
+        }
+}
+
+var collegeRank = [0, 0, 0, 0, 0]
+var collegeList = ['인사대', '경영대', '이공대', '예술대', '신학대']
+var rank1st = 0;
+function checkRankReversal(col) {
+    if (collegeRank[col] > collegeRank[rank1st]) {
+        var str = collegeList[col] + '가 ' + collegeList[rank1st] + '를 제치고 1등으로 역전!';
+        for (var key in openChatDict) {
+            sendTextMessage(key, str);
+        }
+        rank1st = col;
+    }
+}
+
+function rankingGame(senderId, col) {
+    collegeRank[col] += 1;
+    checkRankReversal(col);
+    sendTextMessage(senderId, collegeList[col] + "의 현재 점수는 " + collegeRank[col] + "점!");
+}
+
+function randomTactGame() {
+    if (Math.floor(Math.random() * 10) == 0) {
+        for (var key in openChatDict) {
+            var str = "[[ 눈치게임 시작!! ]]";
+            sendTextMessage(key, str);
         }
     }
-    else {
-        
-    }*/
+}
+
+function enterOpenChat(senderId, content) {
+    openChatDict[senderId] = content;
+    for (var key in openChatDict) {
+        var str = openChatDict[senderId] + "님이 채팅방에 입장했어요";
+        sendTextMessage(key, str);
+    }
+}
+
+function quitOpenChat(senderId) {
+    if ((senderId in openChatDict)) {
+        delete openChatDict[senderId];
+        for (var key in openChatDict) {
+            var str = openChatDict[senderId] + "님이 채팅방을 나갔어요";
+            sendTextMessage(key, str);
+        }
+    }
+}
+
+function waitOpenChat(senderId) {
+    if (!(senderId in openChatDict) && !(senderId in openChatDict2)) {
+        sendTextMessage(senderId, '닉네임은 뭐로할거에요?');
+        openChatDict2[senderId] = 1;
+    }
 }
 
 // senderId: Nickname
 var openChatDict = {};
 var openChatDict2 = {};
-
-/******
-var randomChatQueue = new Queue();
-var randomChatDict = {};
-function randomChatInit(senderId) {
-    var str = '대기중이야 조금만 기다려!';
-    setTimeout(sendTextMessage, 1, senderId, str);
-    setTimeout(function() {
-        if (randomChatQueue.dataStore.length >= 1) {
-            var user = randomChatQueue.dequeue();
-            sendTextMessage(senderId, '매칭 완료!');
-            sendTextMessage(user, '매칭 완료!');
-        } else {
-            randomChatQueue.enqueue(senderId);
-        }
-    }, 1);
-}
-
-********/
 
 function receivedPostback(event) {
     var senderId = event.sender.id;
